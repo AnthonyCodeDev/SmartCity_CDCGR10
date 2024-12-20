4f769f7b153cf6c1b04ddc4423a7a6750d723649
Panda <46421954+AnthonyCodeDev@users.noreply.github.com>
2024-12-20 11:51:47 +0100

    Update ScriptConfigLinuxLocale.sh

--- ConfigLinuxServLocal/ScriptConfigLinuxLocale.sh
+++ ConfigLinuxServLocal/ScriptConfigLinuxLocale.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-
-# Variables de sauvegarde
-BACKUP_DIR="/backup"  # Dossier de sauvegarde
-WEB_DIR="/server/energie"
-DB_HOST="localhost"
-DB_NAME="db_smartcity_energie"
-DB_USER="root"
-DB_PASSWORD="HEH2/16"
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-
-
 # Mise à jour du système
 echo "Mise à jour du système..."
 sudo dnf update -y
@@ -55,31 +42,3 @@ sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --permanent --add-service=mysql
 sudo firewall-cmd --reload
-
-
-# Sauvegarde
-echo "Création du dossier de sauvegarde..."
-mkdir -p "$BACKUP_DIR"
-
-# Sauvegarde du dossier web
-echo "Sauvegarde du dossier $WEB_DIR..."
-tar -czf "$BACKUP_DIR/web_backup_$DATE.tar.gz" -C "$(dirname "$WEB_DIR")" "$(basename "$WEB_DIR")"
-if [ $? -eq 0 ]; then
-    echo "Sauvegarde du dossier web terminée : $BACKUP_DIR/web_backup_$DATE.tar.gz"
-else
-    echo "Erreur lors de la sauvegarde du dossier web." >&2
-    exit 1
-fi
-
-# Sauvegarde de la base de données
-echo "Sauvegarde de la base de données $DB_NAME..."
-mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_DIR/db_backup_$DATE.sql"
-if [ $? -eq 0 ]; then
-    echo "Sauvegarde de la base de données terminée : $BACKUP_DIR/db_backup_$DATE.sql"
-else
-    echo "Erreur lors de la sauvegarde de la base de données." >&2
-    exit 1
-fi
-
-# Résumé
-echo "Toutes les configurations et sauvegardes sont terminées avec succès."
