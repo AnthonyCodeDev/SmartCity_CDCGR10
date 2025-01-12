6e5ff40ac7ab0a16f90f3019cfc193b79983faa0
Panda <46421954+AnthonyCodeDev@users.noreply.github.com>
2025-01-12 20:41:03 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,8 +9,8 @@ if (!defined('BASE_URL')) {
 
     if ($productionMode == true) {
         // PROD MODE
-        define('TOTAL_URL', "http://192.168.20.1".BASE_URL);
         define('BASE_URL', '/');
+        define('TOTAL_URL', "http://192.168.20.1".BASE_URL);
     } else {
         // DEV MODE
         define('BASE_URL', '/SmartCity_CDCGR10/');
